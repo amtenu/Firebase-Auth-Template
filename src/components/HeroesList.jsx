6dc4e9c6dc50450514bc9ae84f438db193b2529d
amtenu <amannov21@gmail.com>
2022-06-16 11:57:23 -0600

    Changed the getHeroList function by a button to realtime Listner so that database and react interact in realtime

--- src/components/HeroesList.jsx
+++ src/components/HeroesList.jsx
@@ -1,34 +1,29 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import React, { useContext, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../providers/FirebaseProvider";
 
 function HeroesList() {
   const fbContext = useContext(FirebaseContext);
   const db = fbContext.db;
   const [heroes, setHeroes] = useState([]);
-  const getHeroesData = async () => {
-    try {
-      let collectionRef = collection(db, "heroes");
-      let queryRef = query(collectionRef, orderBy("name", "asc"));
-      let quarySnap = await getDocs(queryRef);
-
-      if (quarySnap.empty) {
-        console.log("No Docs found");
+  useEffect(() => {
+    let collectionRef = collection(db, "heroes");
+    let queryRef = query(collectionRef, orderBy("name"));
+    const unsubscribe = onSnapshot(queryRef, (querySnap) => {
+      if (querySnap.empty) {
+        console.log("No Docs Found");
       } else {
-        let heroesData = quarySnap.docs.map((doc) => ({
-          ...doc.data(),
-          DOC_ID: doc.id,
-        }));
+        let heroesData = querySnap.docs.map(doc => 
+          doc.data());
         setHeroes(heroesData);
       }
-    } catch (ex) {
-      console.log("Firesgtore Failure", ex.message);
-    }
-  };
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <div>
-      <button onClick={() => getHeroesData()}>Get Heroes</button>
+     
       <br />
       {heroes.map((hero) => {
         return (
